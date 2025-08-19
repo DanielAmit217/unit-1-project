7144ac44997f6b61919e47c9b1dca944a85aa441
danny amit <danny@iamit.org>
2025-08-19 19:17:59 -0400

    added level,chached new elements,added game functionality, added submit button

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+// variables
 let userSandwich = [];
 let computerSandwich = [];
-// initialize a gloal variable to keep track of ingredient level
 let level = 3;
+let points = 0;
 
-
+// items
 const ingredientsList = {
   1: "Bread",
   2: "Meat",
@@ -15,23 +16,25 @@ const ingredientsList = {
   8: "Ketchup",
 };
 
-
+// cached elements
 const ingredientsBtn = document.querySelectorAll(".button");
-const returnBtn = document.querySelector("button");
 const cuttingBoardPreview = document.querySelector("#cutting-board");
 const orderView = document.querySelector("#order");
+const returnBtn = document.querySelector("#return-button");
+const submitBtn = document.querySelector("#submit-button");
+// const displayLevel = document.querySelector("#")
 
-
+// functions
 function updateOrderUI() {
   let orderString = "";
   computerSandwich.forEach((ingredient) => {
-    orderString += `<p>${ingredient}</p>`
+    orderString += `<p>${ingredient}</p>`;
   });
   orderView.innerHTML = "";
-  orderView.insertAdjacentHTML("beforeend", orderString)
+  orderView.insertAdjacentHTML("beforeend", orderString);
 }
 
-function updateIngredientsUI() {
+function updateCuttingBoardUI() {
   let htmlString = "";
   userSandwich.forEach((ingredient) => {
     htmlString += `<p>${ingredient}</p>`;
@@ -40,9 +43,30 @@ function updateIngredientsUI() {
   cuttingBoardPreview.insertAdjacentHTML("beforeend", htmlString);
 }
 
+function compareSandos() {
+  // user > comp
+  // user < comp
+  // user == comp
+  let smallerSando =
+    userSandwich.length < computerSandwich.length
+      ? userSandwich
+      : computerSandwich;
+  for (let i = 0; i < smallerSando.length; i++) {
+    if (computerSandwich[i] === userSandwich[i]) {
+      // Implement a pointCalculator(pointsObj) function that you pass raw points to
+      // pointsCalculator({comp: 2, user: 3})
+      points++;
+    }
+  }
+  console.log(points);
+  // Clear userSandwich board
+  // Rerender computer sandwich board
+  // Reset timer
+}
 
 function addRandomIngredient() {
-  function getRandomNumber(min, max) { //random number generator (got it from google)
+  function getRandomNumber(min, max) {
+    //random number generator (got it from google)
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -52,30 +76,41 @@ function addRandomIngredient() {
   computerSandwich.push(getRandomIngredient);
 }
 
-function int() {
-  for(let i = 0; i < level; i++) {
-    addRandomIngredient();
-  }
-  level ++
-  updateOrderUI();
-}
-
-
-returnBtn.addEventListener("click", (remove) => {
+// event listeners
+returnBtn.addEventListener("click", () => {
   userSandwich.pop();
-  updateIngredientsUI();
+  updateCuttingBoardUI();
   console.log(userSandwich); // log
+});
+
+submitBtn.addEventListener("click", () => {
+  compareSandos();
+  // console.log(points); // log
+  render();
 });
 
 ingredientsBtn.forEach((button) => {
   button.addEventListener("click", (event) => {
     console.log(`This is the ${event.target.textContent} container`);
     userSandwich.push(event.target.textContent);
-    updateIngredientsUI();
+    updateCuttingBoardUI();
     console.log(userSandwich); // log
   });
 });
 
+function render() {
+  computerSandwich = [];
+  userSandwich = [];
+  for (let i = 1; i <= level; i++) {
+    addRandomIngredient();
+  }
+  level++;
+  updateOrderUI();
+  updateCuttingBoardUI();
+}
 
-int();
-console.log(computerSandwich) // log
+function init() {
+  render();
+}
+
+init();
